fa6c9129ab97f538ae54a3e96ce1e01c14f0791c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-03-17 13:07:22 +0400

    Удалил _config.production.php, чтобы на гитхабе не было моих паролей

--- _config.production.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define("ROOT_FOLDER", "/");
-	define("SERVER", "u361687.mysql.masterhost.ru");
-	define("USERNAME", "u361687");
-	define("PASSWORD", "4retrideeduou");
-	define("DATABASE", "u361687");
-?>
